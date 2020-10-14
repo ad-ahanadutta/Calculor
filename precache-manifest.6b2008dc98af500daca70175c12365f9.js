a5b96366acda4acd881d6e9495c73064b7bb9b9d
ad-ahanadutta <ahanadutta555@gmail.com>
2020-10-15 03:41:20 +0530

    Updates

---
+++ precache-manifest.6b2008dc98af500daca70175c12365f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "868ff00f871b5d42f1daa6c96962e7eb",
+    "url": "/Calculor/index.html"
+  },
+  {
+    "revision": "17e5e9fbbc39c0856404",
+    "url": "/Calculor/static/css/main.360cb8d1.chunk.css"
+  },
+  {
+    "revision": "c6e948053e03a92e65f1",
+    "url": "/Calculor/static/js/2.be65834e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculor/static/js/2.be65834e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e5e9fbbc39c0856404",
+    "url": "/Calculor/static/js/main.fd185d53.chunk.js"
+  },
+  {
+    "revision": "336488fa81a6ca83c8cb",
+    "url": "/Calculor/static/js/runtime-main.c7f55f3c.js"
+  }
+]);
